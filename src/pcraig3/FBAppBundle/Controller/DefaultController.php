a8ed6606ded1061b8debeb2a29a8ecd0718f92ee
Paul Craig <pcraig3@uwo.ca>
2015-01-19 04:29:28 -0500

    Added a filler until we have something better login page.

--- src/pcraig3/FBAppBundle/Controller/DefaultController.php
+++ src/pcraig3/FBAppBundle/Controller/DefaultController.php
@@ -8,10 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/home", name="fb_home")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="fb_login")
+     */
+    public function loginAction()
+    {
+        return $this->render('FBAppBundle:Default:login.html.twig');
     }
 }
